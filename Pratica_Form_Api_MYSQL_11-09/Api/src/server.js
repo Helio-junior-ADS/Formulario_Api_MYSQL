e650635dde68ece2f1f06041d47dbb3db155efba
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-09-18 13:45:51 -0300

    Pratica Backend APi crud com paginação

--- Pratica_Form_Api_MYSQL_11-09/Api/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-const db = require('./db/models/index')
-
-app.listen(3333,()=>{
-  console.log(`SERVER ONLINE NA PATH http://localhost:3333`);
-});
---
+++ Pratica_Form_Api_MYSQL_18-09/src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.listen(3333, () => {
+  console.log(`Serve ONLINE na PATH http://localhost:3333`);
+});
+
