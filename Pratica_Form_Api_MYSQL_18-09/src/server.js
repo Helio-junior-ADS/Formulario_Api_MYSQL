905a2f6408a23ae8dad5fd4467c2508fff279efe
Helio-junior-ADS <64769193+Helio-junior-ADS@users.noreply.github.com>
2023-09-25 15:01:50 -0300

    Pratica bankend/api-restfull/mysql/sequelize-cli

--- Pratica_Form_Api_MYSQL_18-09/src/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-app.listen(3333, () => {
-  console.log(`Serve ONLINE na PATH http://localhost:3333`);
-});
-
---
+++ Pratica_Form_Api_MYSQL_25-09/Api/src/server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+app.listen(3333,()=>{
+  console.log('SERVER ONLINE IN PATH http://localhost:3333/users');
+});
+
